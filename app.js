f1c5415981e25aafaf390686b574d9b2ba5fc297
BERKAY GÜL <berrkaygul@gmail.com>
2022-10-28 16:27:41 +0300

    pages created ejs installed

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 
+//templete engine
+app.set("view engine","ejs");
+app.use(express.static("public"));
+
 app.get("/",(req,res)=>{
-    res.send("merhabaa")
+    res.status(200).render('index',{
+        pageName:"index"
+    })
+});
+
+app.get("/about",(req,res)=>{
+    res.status(200).render('about',{
+        pageName:"about"
+    })
 })
 
 app.listen(3000,()=>{
